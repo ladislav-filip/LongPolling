c434fdac6adffeb1bfea3e72296ba12873ed62bc
LF <ladislav.filip@gmail.com>
2021-11-05 18:02:03 +0100

    Inicializace

---
+++ Server/Infratructure/NotifyHub.cs
@@ -0,0 +1,19 @@
+﻿#region Info
+// FileName:    NotifyHub.cs
+// Author:      Ladislav Filip
+// Created:     05.11.2021
+#endregion
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Infratructure
+{
+    public class NotifyHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync(method: "message", "anonymous", "Date");
+        }
+    }
+}
